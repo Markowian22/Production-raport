48a62636151a474b7fbe28e2841e6a0a949ae83f
Markowian22 <120415395+Markowian22@users.noreply.github.com>
2023-12-08 17:22:19 +0100

    zmiana

---
+++ main.py
@@ -0,0 +1,17 @@
+## generate  a function with range 
+
+
+from datetime import datetime
+
+def marek():
+    print("asdasd")
+
+def marek():
+    x = 10
+    for i in range(x):
+        print(i)
+        print()
+
+        
+
+marek()
